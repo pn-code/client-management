5dd7e42c4d3d9c9bbd7ed2e03eed09170251bb76
Philip Nguyen <philipnguyen953@gmail.com>
2024-01-03 13:11:20 -0800

    Added exclamation icon

--- client/src/routes/NotFound.jsx
+++ client/src/routes/NotFound.jsx
@@ -4,9 +4,12 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
     return (
         <div className="full-screen center flex-col">
+            <FaExclamationTriangle color="#cf2102" size={100} />
             <h1>404</h1>
             <h2>Page Not Found</h2>
-            <p className="text-slate-600 mb-4">Sorry, this page does not exist</p>
+            <p className="text-slate-600 mb-4">
+                Sorry, this page does not exist
+            </p>
             <Link className="btn btn--primary" to="/">
                 Return Home
             </Link>
